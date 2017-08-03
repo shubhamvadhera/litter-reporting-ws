81ceba0f9a365ccb2963d5f14d3c4b177ca9ae2e
svadhera <shubham.vadhera@elliemae.com>
2017-08-02 20:34:52 -0700

    - integrated with datamodel to detch litter type from image - update the user report with the new category

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
 var UserReportModel = require('../models/userreportmodel');
 var UserDetailsModel = require('../models/userdetailsmodel');
+
+var dataModelURL = 'http://35.192.190.62:5000/categorize'
 
 /********** User APIs **********/
 
 //GET user details
-router.get('/:userid', function(req, res, next) {
-    UserDetailsModel.findById(req.params.userid, function(err, userdetails) {
+router.get('/:userid', function (req, res, next) {
+    UserDetailsModel.findById(req.params.userid, function (err, userdetails) {
         if (err || !userdetails)
             res.json({
                 status: 404,
@@ -20,8 +24,8 @@ router.get('/:userid', function(req, res, next) {
 });
 
 //DELETE user
-router.delete('/:userid', function(req, res, next) {
-    UserDetailsModel.findByIdAndRemove(req.params.userid, function(err, userdetails) {
+router.delete('/:userid', function (req, res, next) {
+    UserDetailsModel.findByIdAndRemove(req.params.userid, function (err, userdetails) {
         if (err || !userdetails)
             res.json({
                 status: 500,
@@ -37,17 +41,17 @@ router.delete('/:userid', function(req, res, next) {
 });
 
 //UPDATE User details
-router.put('/:userid', function(req, res, next) {
-    UserDetailsModel.findById(req.params.userid, function(err, userdetails) {
+router.put('/:userid', function (req, res, next) {
+    UserDetailsModel.findById(req.params.userid, function (err, userdetails) {
         if (err || !userdetails)
             res.json({
                 status: 400,
                 message: 'Error getting user',
                 details: err
             }); else {
-            if(req.body.deviceid) userdetails.deviceid = req.body.deviceid;
-            if(req.body.email) userdetails.email = req.body.email;
-            if(req.body.name) userdetails.name = req.body.name;
+            if (req.body.deviceid) userdetails.deviceid = req.body.deviceid;
+            if (req.body.email) userdetails.email = req.body.email;
+            if (req.body.name) userdetails.name = req.body.name;
 
             userdetails.save(function (err, saveduser) {
                 if (err || !saveduser)
@@ -69,9 +73,9 @@ router.put('/:userid', function(req, res, next) {
 });
 
 //POST save the user
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
     var userDetails = new UserDetailsModel();
-    if(!req.body.name || !req.body.email || !req.body.deviceid)
+    if (!req.body.name || !req.body.email || !req.body.deviceid)
         res.json({
             status: 400,
             message: 'Error saving user',
@@ -80,7 +84,7 @@ router.post('/', function(req, res) {
         userDetails.name = req.body.name;
         userDetails.email = req.body.email;
         userDetails.deviceid = req.body.deviceid;
-        userDetails.save(function(err, savedUser) {
+        userDetails.save(function (err, savedUser) {
             if (err || !savedUser)
                 res.json({
                     status: 500,
@@ -100,9 +104,9 @@ router.post('/', function(req, res) {
 /********** Report APIs **********/
 
 //GET all reports by status
-router.get('/reports/:status', function(req, res, next) {
+router.get('/reports/:status', function (req, res, next) {
     var status = req.params.status;
-    if(status === 'all') {
+    if (status === 'all') {
         UserReportModel.find({}, function (err, docs) {
             if (err || !docs)
                 res.json({
@@ -116,15 +120,15 @@ router.get('/reports/:status', function(req, res, next) {
     } else if (status === 'not-closed') {
         UserReportModel.find(
             {$or: [{status: "open"}, {status: "in-progress"}]}, function (err, docs) {
-            if (err || !docs)
-                res.json({
-                    status: 404,
-                    message: 'Error getting reports',
-                    details: err
-                }); else {
-                res.json(docs);
-            }
-        });
+                if (err || !docs)
+                    res.json({
+                        status: 404,
+                        message: 'Error getting reports',
+                        details: err
+                    }); else {
+                    res.json(docs);
+                }
+            });
     } else {
         UserReportModel.find({status: status}, function (err, docs) {
             if (err || !docs)
@@ -140,7 +144,7 @@ router.get('/reports/:status', function(req, res, next) {
 });
 
 //GET all reports by a user
-router.get('/:userid/reports', function(req, res, next) {
+router.get('/:userid/reports', function (req, res, next) {
     UserReportModel.find({userid: req.params.userid}, function (err, docs) {
         if (err || !docs)
             res.json({
@@ -154,8 +158,8 @@ router.get('/:userid/reports', function(req, res, next) {
 });
 
 // GET the report for the report id
-router.get('/report/:reportid', function(req, res, next) {
-    UserReportModel.findById(req.params.reportid, function(err, userreport) {
+router.get('/report/:reportid', function (req, res, next) {
+    UserReportModel.findById(req.params.reportid, function (err, userreport) {
         if (err || !userreport)
             res.json({
                 status: 404,
@@ -168,8 +172,8 @@ router.get('/report/:reportid', function(req, res, next) {
 });
 
 // DELETE the report for the report id
-router.delete('/report/:reportid', function(req, res, next) {
-    UserReportModel.findByIdAndRemove(req.params.reportid, function(err, userreport) {
+router.delete('/report/:reportid', function (req, res, next) {
+    UserReportModel.findByIdAndRemove(req.params.reportid, function (err, userreport) {
         if (err || !userreport)
             res.json({
                 status: 404,
@@ -185,25 +189,25 @@ router.delete('/report/:reportid', function(req, res, next) {
 });
 
 //PUT update the report
-router.put('/report/:reportid', function(req, res) {
-    UserReportModel.findById(req.params.reportid, function(err, userreport) {
+router.put('/report/:reportid', function (req, res) {
+    UserReportModel.findById(req.params.reportid, function (err, userreport) {
         if (err || !userreport)
             res.json({
                 status: 404,
                 message: 'Error getting user report',
                 details: err
             }); else {
-            if(req.body.submitted) userreport.submitted = req.body.submitted;
-            if(req.body.location) {
+            if (req.body.submitted) userreport.submitted = req.body.submitted;
+            if (req.body.location) {
                 if (req.body.location.lat) userreport.location.lat = req.body.location.lat;
                 if (req.body.location.lng) userreport.location.lng = req.body.location.lng;
             }
-            if(req.body.address) userreport.address = req.body.address;
-            if(req.body.description) userreport.description = req.body.description;
-            if(req.body.littertype) userreport.littertype = req.body.littertype;
-            if(req.body.priority) userreport.priority = req.body.priority;
-            if(req.body.status) userreport.status = req.body.status;
-            if(req.body.image) userreport.image = req.body.image;
+            if (req.body.address) userreport.address = req.body.address;
+            if (req.body.description) userreport.description = req.body.description;
+            if (req.body.littertype) userreport.littertype = req.body.littertype;
+            if (req.body.priority) userreport.priority = req.body.priority;
+            if (req.body.status) userreport.status = req.body.status;
+            if (req.body.image) userreport.image = req.body.image;
 
             userreport.save(function (err, savedReport) {
                 if (err || !savedReport)
@@ -224,9 +228,9 @@ router.put('/report/:reportid', function(req, res) {
 });
 
 //POST save the report for a user
-router.post('/report/', function(req, res) {
+router.post('/report/', function (req, res) {
     var userReport = new UserReportModel();
-    if(!req.body.userid || !req.body.submitted || !req.body.location || !req.body.address || !req.body.priority || !req.body.status || !req.body.image)
+    if (!req.body.userid || !req.body.submitted || !req.body.location || !req.body.address || !req.body.priority || !req.body.status || !req.body.image)
         res.json({
             status: 400,
             message: 'Error saving user',
@@ -238,11 +242,9 @@ router.post('/report/', function(req, res) {
         userReport.location.lng = req.body.location.lng;
         userReport.address = req.body.address;
         userReport.description = req.body.description;
-        // userReport.littertype = req.body.littertype;
         userReport.priority = req.body.priority;
         userReport.status = req.body.status;
         userReport.image = req.body.image;
-
         userReport.save(function (err, savedReport) {
             if (err || !savedReport)
                 res.json({
@@ -255,9 +257,50 @@ router.post('/report/', function(req, res) {
                     message: 'User report created !',
                     details: savedReport
                 });
+                // send image for processing to data model and save response litter type in DB
+                setLitterTypeFromImage(req.body.image, savedReport._id);
             }
         });
     }
 });
+
+//calls the data model to get the catefory and update in DB
+function setLitterTypeFromImage(image, reportid) {
+    console.log("Image processing request for report: " + reportid);
+    request.post({
+        url: dataModelURL,
+        json: {image: image},
+        timeout: 8000
+    }, function (error, res, body) {
+        if (error || body.status !== 200) {
+            console.log("Error getting litter type from data model: ");
+            console.log(error);
+        } else {
+            console.log("Category from image detected - " + body.category);
+
+            //update the report with the category detected
+            updateUserReportWithCategory(reportid,body.category);
+        }
+    });
+}
+
+//updates the report in DB with new category
+function updateUserReportWithCategory(reportid, category) {
+    UserReportModel.findById(reportid, function (err, userreport) {
+        if (err || !userreport)
+            console.error("Error finding report " + reportid + " Error: " + err);
+        else {
+            userreport.littertype = category;
+            userreport.save(function (err, savedReport) {
+                if (err || !savedReport) {
+                    console.error("Error saving report " + reportid + " with detected category: " + category + "Error: " + err);
+                }
+                else {
+                    console.log("Report " + reportid + " updated with detected category");
+                }
+            });
+        }
+    });
+}
 
 module.exports = router;
